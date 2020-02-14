b03568f7a410849c5b3ca84ae0cd010a03517d67
liusongsen <245799856@qq.com>
2020-02-14 17:13:39 +0800

    删除比不要的类库

--- test/test.php
+++ test/test.php
@@ -9,7 +9,32 @@
 
 include "../vendor/autoload.php";
 
-$config = [];
+$config = [
+    //调试模式
+    'debug' => false,
+    //版本
+    'version' => '20170405',
+    //字符编号 GBK|UTF-8
+    'charSet' => 'utf-8',
+    //数据格式，1:json,2:xml格式
+    'dataType' => '2',
+    //日志文件路径
+    'cacheFilePath' => "",
+    //应用ID
+    'appId' => 'bddc8bd7ff8a5794bd4a684a81781d6f',
+    //授权访问token
+    'accessToken' => "167b43eab45c74f9d3c8c7675944d59a",
+    //主账号
+    'accountSid' => "3ef4da60af9aaa29f49625062479629d",
+    //云总机测试平台默认域名
+    'apiHostUrl' => "https://api.emicloud.com/",
+    //云总机
+    'cloudNumber' => "02028269410",
+    //子账号
+    'subAccountSid' => "ce0df13133e6ea1c616fd3fa2aaece0e",
+    //授权访问token
+    'subAccessToken' => "1fa13232580a015f9ca53a992a239f04",
+];
 
 $sdk = new \jzweb\voip\emicnet\Client($config);
 //活在通话语音文件
--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,11 +6,8 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'phpseclib\\' => array($vendorDir . '/phpseclib/phpseclib/phpseclib'),
     'jzweb\\voip\\emicnet\\' => array($baseDir . '/emicnet.com/src'),
     'Psr\\Http\\Message\\' => array($vendorDir . '/psr/http-message/src'),
-    'League\\Flysystem\\Sftp\\' => array($vendorDir . '/league/flysystem-sftp/src'),
-    'League\\Flysystem\\' => array($vendorDir . '/league/flysystem/src'),
     'GuzzleHttp\\Psr7\\' => array($vendorDir . '/guzzlehttp/psr7/src'),
     'GuzzleHttp\\Promise\\' => array($vendorDir . '/guzzlehttp/promises/src'),
     'GuzzleHttp\\' => array($vendorDir . '/guzzlehttp/guzzle/src'),
